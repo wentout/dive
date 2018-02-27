cc93107d086846af1ec1a70e748fcc433f778af4
went.out <went.out@gmail.com>
2018-02-28 01:30:29 +0300

    async hooks addition

---
+++ example.js
@@ -0,0 +1,67 @@
+'use strict';
+
+const dive = require('./index');
+
+const line = '-'.repeat(75);
+console.log('\n\nSTART\n');
+console.log('eid : |  context :  | mark :                    | log args :');
+console.log(line);
+
+const log = (mark, ...args) => {
+	mark = `${mark}`.padEnd(25);
+	const eid = `${dive.getExecId()}`.padStart(5);
+	const ctx = `${global._currentContext}`.padStart(10).padEnd(11);
+	console.log(`${eid} | ${ctx} | ${mark} |`, ...args);
+};
+
+const d = (cb) => {
+	log('d 1');
+	return (t, y) => {
+		log('d 2', t, y);
+		setTimeout(() => {
+			log('d 3', '-> hook');
+			cb('asdf', 'fdsa', () => {
+				log('d 4');
+			});
+		}, 100);
+	};
+};
+
+const z = (cb) => {
+	log('z 1', '-> native');
+	process.nextTick(() => {
+		log('z 2', '-> hook');
+		setImmediate(() => {
+			log('z 3', '-> hook');
+			setTimeout(() => {
+				log('z 4', '-> hook');
+				cb('zxcv', 'vcxz');
+			}, 100);
+		});
+	}, 1000);
+};
+
+const m = dive.call(d, 'One ( 1 )')(function (a, b, cb) {
+	log('m 1', a, b);
+	setTimeout(() => {
+		log('m 2', a, b);
+		setTimeout(() => {
+			log('m 3', a, b);
+			cb();
+		}, 100);
+	}, 100);
+});
+
+const s = dive.call(z, '( 2 ) Two');
+
+s(m);
+d(m)('undef context', 'example');
+
+setTimeout(() => {
+	console.log('\n');
+	console.log('TOTAL');
+	console.log(line);
+	log('dive.hashAllHooks', dive.hashAllHooks);
+	log('global._currentContext');
+	console.log('\n');
+}, 3000);
--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 'use strict';
 
-const dive = function (context, ctx) {
+const dive = function (context, ctx, brfn) {
 	const fn = this;
 	const base = function () {
 		const run = fn.bind(ctx || this);
 		const args = [...arguments].map(arg => {
 			if (typeof arg == 'function') {
-				arg = dive.call(arg, context);
+				arg = dive.call(arg, context, ctx || this, brfn);
 			}
 			return arg;
 		});
@@ -18,21 +18,75 @@ const dive = function (context, ctx) {
 		} else {
 			delete global._currentContext;
 		}
+		if (typeof answer == 'function' && !brfn) {
+			return dive.call(answer, context);
+		}
 		return answer;
 	};
 	base._currentContext = context;
 	return base;
 };
 
-const objectMethodDive = (obj, prop, context) => {
-	return dive.call(obj[prop], context, obj);
+const objectMethodDive = (obj, methodName, context, breakResultFn) => {
+	return dive.call(obj[methodName], context, obj, breakResultFn);
 };
+dive.object = objectMethodDive;
 
-objectMethodDive.enable = () => {
+dive.enable = () => {
 	Function.prototype.dive = dive;
 };
-objectMethodDive.disable = () => {
+dive.disable = () => {
 	delete Function.prototype.dive;
 };
 
-module.exports = objectMethodDive;
+const async_hooks = require('async_hooks');
+const hashAllHooks = {};
+const drop = (asyncId) => {
+	// rolling back
+	if (hashAllHooks[asyncId]) {
+		global._currentContext = hashAllHooks[asyncId].mix;
+		delete hashAllHooks[asyncId];
+	}
+};
+const hooks = {
+	init (asyncId, type, triggerId, resource) {
+		const ctx = global._currentContext;
+		if (ctx) {
+			hashAllHooks[asyncId] = {
+				ctx,
+				type,
+				asyncId,
+				triggerId,
+				resource
+			};
+		}
+	},
+	before (asyncId) {
+		if (hashAllHooks[asyncId]) {
+			const mix = global._currentContext;
+			if (mix) {
+				// rolling forward
+				hashAllHooks[asyncId].mix = mix;
+			}
+			global._currentContext = hashAllHooks[asyncId].ctx;
+		}
+	},
+	after   : drop,
+	destroy : drop
+};
+
+const asyncHook = async_hooks.createHook(hooks);
+asyncHook.enable();
+dive.asyncHooksEnable = () => {
+	asyncHook.enable();
+};
+dive.asyncHooksDisable = () => {
+	hashAllHooks = {};
+	asyncHook.disable();
+};
+dive.hashAllHooks = hashAllHooks;
+dive.getExecId = () => {
+	return async_hooks.executionAsyncId();
+};
+
+module.exports = dive;
