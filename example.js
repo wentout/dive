6883d8417cdbe44e6e73507d080f7cb799b8bcc3
went.out <went.out@gmail.com>
2018-03-04 00:48:56 +0300

    dive context injection improoved

--- example.js
+++ example.js
@@ -1,68 +1,110 @@
 'use strict';
 
+const util = require('util');
 const dive = require('./index');
-dive.enableAsyncHooks();
+
+
+var count = 0;
 
 const line = '-'.repeat(75);
-console.log('\n\nSTART\n');
-console.log('eid : |  context :  | mark :                    | log args :');
-console.log(line);
+const makeReportHead = (mark) => {
+	process.stdout.write(`\n\nSTART ${mark}\n\n`);
+	process.stdout.write('tid -> eid : |  context :  | mark  -> [ trace.step ] : | log args :\n');
+	process.stdout.write(`${line}\n`);
+};
 
 const log = (mark, ...args) => {
 	mark = `${mark}`.padEnd(25);
-	const eid = `${dive.getExecId()}`.padStart(5);
+	const eid = `${dive.tid} -> ${dive.eid}`.padStart(12);
 	const ctx = `${dive.currentContext}`.padStart(10).padEnd(11);
-	console.log(`${eid} | ${ctx} | ${mark} |`, ...args);
+	process.stdout.write(`\n${eid} | ${ctx} | ${mark} | ${args.join(' ')}`);
 };
 
-const d = (cb) => {
-	log('d 1');
+const d = (cb, trace = ++count) => {
+	trace = `${trace}`.padStart(2);
+	log(`d -> [ ${trace} . 1 ]`);
 	return (t, y) => {
-		log('d 2', t, y);
+		log(`d -> [ ${trace} . 2 ]`, t, y);
 		setTimeout(() => {
-			log('d 3', '-> hook');
-			cb('asdf', 'fdsa', () => {
-				log('d 4');
+			log(`d -> [ ${trace} . 3 ]`, '-> hook');
+			cb('d-a1', 'd-a2', (look) => {
+				// this will never run in d context
+				// cause it will run in cb context
+				log(`d -> [ ${trace} . 4 ]`, look);
 			});
 		}, 100);
 	};
 };
 
-const z = (cb) => {
-	log('z 1', '-> native');
+const z = (cb, trace = ++count) => {
+	trace = `${trace}`.padStart(2);
+	log(`z -> [ ${trace} . 1 ]`, '-> native');
 	process.nextTick(() => {
-		log('z 2', '-> hook');
+		log(`z -> [ ${trace} . 2 ]`, '-> hook 1');
 		setImmediate(() => {
-			log('z 3', '-> hook');
+			log(`z -> [ ${trace} . 3 ]`, '-> hook 2');
+			const tpcall = () => {
+				log(`z -> [ ${trace} . 4 ]`, '-> hook 3');
+				cb('z-a1', 'z-a2');
+			};
+			setTimeout(tpcall, 100);
 			setTimeout(() => {
-				log('z 4', '-> hook');
-				cb('zxcv', 'vcxz');
-			}, 100);
+				log(`z -> [ ${trace} . 5 ]`, '-> hook 4');
+				cb('z-a3', 'z-a4');
+			}, 200);
 		});
 	}, 1000);
 };
 
-const m = dive.call(d, 'One ( 1 )')(function (a, b, cb) {
-	log('m 1', a, b);
+const w = function (a, b, cb, trace = ++count) {
+	trace = `${trace}`.padStart(2);
+	log(`w -> [ ${trace} . 1 ]`, a, b);
 	setTimeout(() => {
-		log('m 2', a, b);
+		log(`w -> [ ${trace} . 2 ]`, a, b);
 		setTimeout(() => {
-			log('m 3', a, b);
-			cb();
+			log(`w -> [ ${trace} . 3 ]`, a, b);
+			cb('aw4m');
 		}, 100);
 	}, 100);
+};
+
+const test = (mark, cb) => {
+	
+	makeReportHead(mark);
+	
+	const m = dive.call(d, 'One ( 1 )')(w);
+	
+	const s = dive.call(z, '( 2 ) Two');
+	
+	// no context
+	d((n1, n2, cb) => {
+		log('w \\ o', n1, n2);
+		cb('w \\ o');
+	})('w \\ o', 'context');
+	
+	// dive -> context
+	s(m);
+	
+	// binded context without dive
+	d(m)('binded', 'context');
+	
+	setTimeout(() => {
+		console.log('\n');
+		console.log(`TOTAL ${mark}`);
+		console.log(line);
+		log('dive.asyncIdHooks', util.inspect(dive.asyncIdHooks), '\n');
+		log('dive.triggerHooks', util.inspect(dive.triggerHooks), '\n');
+		log('dive.currentContext', dive.currentContext);
+		console.log('\n');
+		if (typeof cb == 'function') cb();
+	}, 3000);
+	
+};
+
+test('no async_hooks', () => {
+	dive.enableAsyncHooks();
+	test('async_hooks enabled');
 });
 
-const s = dive.call(z, '( 2 ) Two');
+	
 
-s(m);
-d(m)('undef context', 'example');
-
-setTimeout(() => {
-	console.log('\n');
-	console.log('TOTAL');
-	console.log(line);
-	log('dive.hashAllHooks', dive.hashAllHooks);
-	log('dive.currentContext');
-	console.log('\n');
-}, 3000);
--- index.js
+++ index.js
@@ -6,7 +6,6 @@
  * please use the link below for notice
  * https://github.com/wentout/dive
  * 
- * 
  *   ABOUT
  * 
  * It utilize the ability of patching function callbacks
@@ -17,13 +16,55 @@
  */
 
 var currentContext;
+const changeContext = (value) => {
+	currentContext = value;
+	return value;
+};
+
+const state = {
+	// [asyncId] == ctx
+	asyncIdHooks : {},
+	
+	// [triggerId][asyncId] == ctx
+	triggerHooks : {},
+	// [eid][asyncId] == ctx
+	eidHooks     : {},
+	// [tid][asyncId] == ctx
+	tidHooks     : {},
+	
+	baseRunning  : false,
+	hookRunning  : false,
+	
+	// about to additionally enable
+	// eids for context selection
+	eidsEnabled  : false,
+	
+	DEBUGING_ON  : false
+};
+
+Object.defineProperty(state, 'currentContext', {
+	get () {
+		return currentContext;
+	},
+	set (value) {
+		return changeContext(value);
+	},
+	configurable : false,
+	enumerable   : false
+});
 
 // basic functionality
 const dive = function (context, ctx, brfn, ..._args) {
-	if (context === undefined) {
-		context = currentContext;
-	}
 	let fn = this;
+	if (context === undefined) {
+		if (typeof fn == 'function') {
+			// this call from Function.prototype.dive
+			// so we just need to get current context
+			context = state.currentContext;
+		} else {
+			return new Error('context-dive dive context is undefined');
+		}
+	}
 	if (typeof fn !== 'function') {
 		// Call as an Object Property
 		if (context && typeof context[ctx] == 'function') {
@@ -40,11 +81,14 @@ const dive = function (context, ctx, brfn, ..._args) {
 			}
 			return arg;
 		});
-		const prevContext = currentContext;
-		currentContext = base._currentContext;
+		const prevContext = state.currentContext;
+		state.currentContext = base._currentContext;
+		state.baseRunning = true;
 		const answer = run(...args);
-		currentContext = prevContext;
+		state.baseRunning = false;
+		state.currentContext = prevContext;
 		if (typeof answer == 'function' && !brfn) {
+			// return dive.call(answer, context || prevContext);
 			return dive.call(answer, context);
 		}
 		return answer;
@@ -57,37 +101,53 @@ const dive = function (context, ctx, brfn, ..._args) {
 	return base;
 };
 
+['currentContext', 'asyncIdHooks', 'triggerHooks', 'eidsEnabled'].forEach(name => {
+	Object.defineProperty(dive, name, {
+		get () {
+			return state[name];
+		},
+		configurable : false,
+		enumerable   : true
+	});	
+});
 
 dive.enableFunctions = () => {
+	if (Function.prototype.dive) {
+		return;
+	}
 	Object.defineProperty(Function.prototype, 'dive', {
 		get () {
-			return dive.call(this, currentContext);
+			return dive.call(this);
 		},
 		enumerable   : true
 	});	
 	Function.prototype._dive = dive;
 };
+
 dive.disableFunctions = () => {
 	delete Function.prototype.dive;
 	delete Function.prototype._dive;
 };
 
-const setCurrentContextProp = (obj) => {
-	Object.defineProperty(obj, 'currentContext', {
+dive.useGlobal = () => {
+	Object.defineProperty(global, 'currentContext', {
 		get () {
-			return currentContext;
+			return state.currentContext;
 		},
 		configurable : false,
 		enumerable   : true
 	});
 };
-dive.setCurrentContextProp = setCurrentContextProp;
-setCurrentContextProp(dive);
-dive.useGlobal = () => {
-	setCurrentContextProp(global);
+
+dive.enableEIDs = () => {
+	state.eidsEnabled = true;
 };
+dive.disableEIDs = () => {
+	state.eidsEnabled = false;
+};
+
 dive.put = (context) => {
-	currentContext = context;
+	state.currentContext = context;
 };
 
 
@@ -95,63 +155,221 @@ dive.put = (context) => {
 // async hooks
 const async_hooks = require('async_hooks');
 
-const hashAllHooks = {};
-Object.defineProperty(dive, 'hashAllHooks', {
-	get () {
-		return hashAllHooks;
-	},
-	configurable : false,
-	enumerable   : true
-});
-
-const drop = (asyncId) => {
-	// rolling back
-	if (hashAllHooks[asyncId]) {
-		currentContext = hashAllHooks[asyncId].mix;
-		delete hashAllHooks[asyncId];
+const saveHooksContext = (ctx) => {
+	
+	const asyncId   = ctx.asyncId;
+	const triggerId = ctx.triggerId;
+	
+	if (!state.asyncIdHooks[ctx.asyncId]) {
+		state.asyncIdHooks[ctx.asyncId] = ctx;
+	} else if (state.DEBUGING_ON) {
+		debugger;
 	}
+	
+	if (!state.triggerHooks[triggerId]) {
+		state.triggerHooks[triggerId] = {};
+	}
+	if (!state.triggerHooks[triggerId][asyncId]) {
+		state.triggerHooks[triggerId][asyncId] = ctx;
+	} else if (state.DEBUGING_ON) {
+		debugger;
+	}
+	
+	// tid & eid
+	const eid = dive.eid;
+	const tid = dive.tid;
+	
+	if(! state.eidHooks[eid]) {
+		state.eidHooks[eid] = {};
+	}
+	if (!state.eidHooks[eid][asyncId]) {
+		state.eidHooks[eid][asyncId] = ctx;
+	} else if (state.DEBUGING_ON) {
+		debugger;
+	}
+	
+	if(! state.tidHooks[tid]) {
+		state.tidHooks[tid] = {};
+	}
+	if (!state.tidHooks[tid][asyncId]) {
+		state.tidHooks[tid][asyncId] = ctx;
+	} else if (state.DEBUGING_ON) {
+		debugger;
+	}
+
 };
-const hooks = {
-	init (asyncId, type, triggerId, resource) {
-		const ctx = currentContext;
-		if (ctx !== undefined) {
-			hashAllHooks[asyncId] = {
-				ctx,
-				type,
+
+const init = (asyncId, type, triggerId, resource) => {
+	
+	if (state.currentContext !== undefined) {
+		
+		// if we have some context
+		// we will save it to hooks storage
+		const ctx = {
+			
+			// easy, we definetely
+			// have to dive deeper
+			// with this upon the stage
+			ctx : state.currentContext,
+			
+			// how many context were nested
+			// from this current context
+			probability : 0,
+			
+			// our tracking IDs
+			asyncId,
+			triggerId,
+			
+			// additional debugging data,
+			// may be we need too look on
+			type,
+			resource,
+			
+		};
+		saveHooksContext(ctx);
+		
+	} else {
+		
+		const prevId = asyncId - 1;
+		
+		// maybe new context born from calls
+		var it = state.asyncIdHooks[prevId];
+		
+		// this asyncId does not exists
+		if (
+			!state.asyncIdHooks[asyncId] &&
+			// but previous execution does
+			// and this run is directly after
+			it && it.resource &&
+			// next tick hop has no callback
+			!it.resource.callback &&
+			// and if probability == 0
+			// it means it is still 
+			// inside of our context ?
+			it.probability == 0
+		) {
+			
+			const ctx = {
+				
+				// follow the previous
+				ctx         : it.ctx,
+				
+				// but increase the probability
+				probability : it.probability + 1,
+				
 				asyncId,
 				triggerId,
-				resource
+				
+				type,
+				resource,
+				
 			};
+			saveHooksContext(ctx);
+			
+		} else if (state.DEBUGING_ON) {
+			debugger;
 		}
-	},
-	before (asyncId) {
-		if (hashAllHooks[asyncId]) {
-			// rolling forward
-			const mix = currentContext;
-			hashAllHooks[asyncId].mix = currentContext;
-			currentContext = hashAllHooks[asyncId].ctx;
+	}
+};
+
+
+const before = (asyncId) => {
+	// roll up everything
+	const eid = dive.eid;
+	let it = state.asyncIdHooks[asyncId];
+	if (!it && dive.eidsEnabled){
+		it = state.asyncIdHooks[eid][asyncId];
+	}
+	if (it) {
+		// here we have context
+		// and able to apply it to new code
+		state.hookRunning = true;
+		// rolling forward
+		const mix = state.currentContext;
+		it.mix = state.currentContext;
+		state.currentContext = it.ctx;
+	}
+};
+
+const drop = (asyncId) => {
+	
+	// 1. rolling back hook running state
+	state.hookRunning = false;
+	
+	// WOW !
+	// at AsyncHook.drop (/home/went/_dev/dive/index.js:293:26)
+	// at emitAfterNative (internal/async_hooks.js:163:40)
+	
+	// seekeing stored hooks
+	const aidHook = state.asyncIdHooks[asyncId];
+	
+	if (aidHook) {
+		state.currentContext = state.asyncIdHooks[asyncId].mix;
+		const triggerId = state.asyncIdHooks[asyncId].triggerId;
+		delete state.asyncIdHooks[asyncId];
+		if (
+			state.triggerHooks[triggerId] &&
+			state.triggerHooks[triggerId][asyncId]
+		) {
+			delete state.triggerHooks[triggerId][asyncId];
 		}
-	},
+	}
+	
+	if (state.triggerHooks[asyncId]) {
+		delete state.triggerHooks[asyncId];
+	}
+	
+	// tid & eid
+	const eid = dive.eid;
+	const tid = dive.tid;
+	
+	if (state.eidHooks[eid] && state.eidHooks[eid][asyncId]) {
+		delete state.eidHooks[eid][asyncId];
+	} else if (state.DEBUGING_ON) {
+		debugger;
+	}
+	if (state.tidHooks[tid] && state.tidHooks[tid][asyncId]) {
+		delete state.tidHooks[tid][asyncId];
+	} else if (state.DEBUGING_ON) {
+		debugger;
+	}
+	
+};
+
+dive.hooks = {
+	init,
+	before,
 	after   : drop,
 	destroy : drop
 };
 
 dive.asyncHook = null;
 if (async_hooks) {
-	const asyncHook = async_hooks.createHook(hooks);
+	const asyncHook = async_hooks.createHook(dive.hooks);
 	dive.asyncHook = asyncHook;
+	dive.async_hooks = async_hooks;
 	
-	const getExecId = () => {
-		return async_hooks.executionAsyncId();
-	};
+	Object.defineProperty(dive, 'eid', {
+		get () {
+			return async_hooks.executionAsyncId();
+		},
+		configurable : false,
+		enumerable   : true
+	});
+	Object.defineProperty(dive, 'tid', {
+		get () {
+			return async_hooks.triggerAsyncId();
+		},
+		configurable : false,
+		enumerable   : true
+	});
+	
 	dive.enableAsyncHooks = () => {
 		asyncHook.enable();
-		dive.getExecId = getExecId;
 		return dive;
 	};
 	dive.disableAsyncHooks = () => {
-		hashAllHooks = {};
-		delete dive.getExecId;
+		state.asyncIdHooks = {};
 		asyncHook.disable();
 		return dive;
 	};
